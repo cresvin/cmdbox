bd3299ace2afd80108031e207eb5c5b70885aaeb
cresvin <oliwiertkacz2@gmail.com>
2026-01-15 13:54:12 +0100

    externalize dependencies in rolldown

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "rolldown";
 
-const isDev = process.env.DEV === "true";
-
 export default defineConfig({
   input: "src/cmdbox.ts",
   output: {
     cleanDir: true,
-    minify: !isDev,
     intro: "#!/usr/bin/env node",
   },
+  external: /^[^./]/,
 });
